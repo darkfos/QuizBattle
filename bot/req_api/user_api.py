23dcba53f0f744187ef1e70ae8ed3576691e6970
darkfos <darkfos82@gmail.com>
2024-04-29 01:30:17 +0300

    essage=Added UserAPI methods

--- bot/req_api/user_api.py
+++ bot/req_api/user_api.py
@@ -1,7 +1,8 @@
 import requests
-from bot.req_api.user_set import UserAuthSet
+from bot.req_api.user_set import user_auth_set
 from api.backend.schemas.UserPDSchema import AddNewUserPDSchema
 from app_settings import tg_settings
+from typing import Union
 
 
 class UserApi:
@@ -24,3 +25,38 @@ class UserApi:
             return True
         else:
             return False
+        
+    async def create_access_token(self, telegram_id: int) -> None:
+        """
+            Creating a new token
+        """
+        
+        result = self.session_req.post(
+            url=self.url+f"auth/create_token/{telegram_id}"
+        )
+        #cookie
+        cookie_result = result.cookies
+
+        if result.status_code == 201:
+            res = dict(result.json())
+            user_auth_set.token = res.get("token")
+            user_auth_set.refresh_token = cookie_result.get("refresh_token")
+        else:
+            return False
+        
+    async def generate_new_token(self, token: str) -> None
+        """
+        Generate new token with help refresh_token
+        """
+
+        result = self.session_req.post(
+            url=self.url+"auth/refresh_token",
+            params={
+                "token": token
+            }
+        )
+
+        if result.status_code == 200:
+            user_auth_set.token = dict(result.json()).get("token")
+        else:
+            return "Не удалось получить новый token"
--- bot/req_api/user_set.py
+++ bot/req_api/user_set.py
@@ -5,3 +5,6 @@ class UserAuthSet:
     def __init__(self) -> None:
         self.token: str = ":"
         self.refresh_token: str = ":"
+
+
+user_auth_set = UserAuthSet()
